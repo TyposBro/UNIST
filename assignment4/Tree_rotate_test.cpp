71c60bb3f777167de3a59d4567ab134b30df2921
ssl ta <happyunistssl@gmail.com>
2021-11-29 13:51:10 +0900

    Fix Tree_rotate_test in assignment4

--- assignment4/Tree_rotate_test.cpp
+++ assignment4/Tree_rotate_test.cpp
@@ -5,36 +5,30 @@
 int main() {
     Tree_t<int, int> t;
 
-    t.insert_internal(10, 10);
-    auto pivot = t.insert_internal(20, 20);
-    t.insert_internal(30, 30);
+    auto a_pivot = t.insert_internal(10, 10);
+    auto b_pivot = t.insert_internal(20, 20);
+    auto c_pivot = t.insert_internal(30, 30);
     std::cout << t.to_string_pre_order() << '\n';
     std::cout << t.to_string_level_order() << '\n';
 
-    t.rotate(pivot);
+    t.rotate(a_pivot, false);
     std::cout << t.to_string_pre_order() << '\n';
     std::cout << t.to_string_level_order() << '\n';
 
     t.insert(5, 5);
     t.insert(15, 15);
     t.insert(25, 25);
-    auto leaf = t.insert_internal(35, 35);
+    t.insert_internal(35, 35);
 
-    pivot = t.insert_internal(10, 11);
+    t.insert_internal(10, 11);
     std::cout << t.to_string_pre_order() << '\n';
     std::cout << t.to_string_level_order() << '\n';
 
-    t.rotate(pivot);
+    t.rotate(b_pivot, true);
     std::cout << t.to_string_pre_order() << '\n';
     std::cout << t.to_string_level_order() << '\n';
 
-    // rotate on head: do nothing
-    t.rotate(pivot);
-    std::cout << t.to_string_pre_order() << '\n';
-    std::cout << t.to_string_level_order() << '\n';
-
-    // rotate on leaf: be sure to handle grandparent
-    t.rotate(leaf);
+    t.rotate(c_pivot, false);
     std::cout << t.to_string_pre_order() << '\n';
     std::cout << t.to_string_level_order() << '\n';
 
