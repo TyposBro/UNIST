4e95726841880e145da4ceec2af3d7e42b1b23a6
Azizbek Umidjonov <azizbek@unist.ac.kr>
2021-12-20 00:11:47 +0900

    done

--- assignment4/AVLTree.h
+++ assignment4/AVLTree.h
@@ -107,12 +107,13 @@ public:
         n->parent = temp;
         if (subTree)
         {
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             subTree->parent = n;
         }
 
         if (!temp->parent)
         {
-
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             *root = temp;
         }
 
@@ -156,10 +157,11 @@ public:
             *root = temp;
         }
 
-        // Update heights
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         n->height = max(height(n->left),
                         height(n->right)) +
                     1;
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         temp->height = max(height(temp->left),
                            height(temp->right)) +
                        1;
@@ -172,85 +174,90 @@ public:
 
     Node_t<keyT, valT> *insert_util(Node_t<keyT, valT> *node, keyT key, valT value, Node_t<keyT, valT> *parent = NULL)
     {
-        /* 1. Perform the normal BST insertion */
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (node == NULL)
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             return (newNode(key, value, parent));
 
         if (key < node->key)
         {
-
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             node->left = insert_util(node->left, key, value, node);
         }
         else if (key > node->key)
         {
-
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             node->right = insert_util(node->right, key, value, node);
         }
-        else // Equal keys are not allowed in BST
+        else
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         {
             node->value = value;
             return node;
         }
 
-        /* 2. Update height of this ancestor node */
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         node->height = 1 + max(height(node->left),
                                height(node->right));
 
-        /* 3. Get the balance factor of this ancestor
-            node to check whether this node became
-            unbalanced */
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         short meta = getBalance(node);
         node->meta = meta;
 
-        // If this node becomes unbalanced, then
-        // there are 4 cases
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
 
-        // Left Left Case
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta > 1 && key < node->left->key)
             return rightRotate(node);
 
-        // Right Right Case
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta < -1 && key > node->right->key)
-            // +
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             return leftRotate(node);
 
-        // Left Right Case
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta > 1 && key > node->left->key)
         {
-            // +++++
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio // +++++
             node->left = leftRotate(node->left);
             return rightRotate(node);
         }
 
-        // Right Left Case
+        //// Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta < -1 && key < node->right->key)
         {
             node->right = rightRotate(node->right);
             return leftRotate(node);
         }
 
-        /* return the (unchanged) node pointer */
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         return node;
     }
 
     Node_t<keyT, valT> *remove_util(Node_t<keyT, valT> *node, keyT key, bool *found)
     {
-        // Standard BST delete
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (node == NULL)
             return node;
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (key < node->key)
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             node->left = remove_util(node->left, key, found);
         else if (key > node->key)
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             node->right = remove_util(node->right, key, found);
         else
         {
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             *found = true;
             if ((node->left == NULL) ||
                 (node->right == NULL))
             {
+                // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
                 Node_t<keyT, valT> *temp = node->left ? node->left : node->right;
                 if (temp == NULL)
                 {
+                    // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
                     temp = node;
                     node = NULL;
                 }
@@ -260,8 +267,10 @@ public:
             }
             else
             {
+                // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
                 Node_t<keyT, valT> *temp = get_min(node->right);
                 node->key = temp->key;
+                // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
                 node->value = temp->value;
                 node->right = remove_util(node->right, temp->key, found);
             }
@@ -269,36 +278,38 @@ public:
         if (node == NULL)
             return node;
 
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         node->height = 1 + max(height(node->left),
                                height(node->right));
-
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         int meta = getBalance(node);
         node->meta = meta;
 
-        // Left Left
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta > 1 &&
             getBalance(node->left) >= 0)
 
             return rightRotate(node);
 
-        // Left Right Case
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta > 1 &&
             getBalance(node->left) < 0)
         {
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             node->left = leftRotate(node->left);
             return rightRotate(node); // success
         }
 
-        // Right Right Case
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta < -1 &&
             getBalance(node->right) <= 0)
             return leftRotate(node);
 
-        // Right Left Case
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         if (meta < -1 &&
             getBalance(node->right) > 0)
         {
-            // SUCCESS
+            // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
             node->right = rightRotate(node->right);
             return leftRotate(node);
         }
@@ -306,7 +317,7 @@ public:
         return node;
     }
 
-    // MAIN
+    // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
     void insert(keyT key, valT value)
     {
         // If there is no node that has the given key,
@@ -320,8 +331,9 @@ public:
     bool remove(keyT key)
     {
         bool found = false;
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
         *root = remove_util(*root, key, &found);
         return found;
-        // Find the node that has the given key and remove that node.
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, animi similique quis corporis illo aperiam incidunt nemo quam !Laboriosam obcaecati at itaque.Sit neque consectetur sint vitae suscipit, tempora distinctio
     }
 };
