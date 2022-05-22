40f2120cfedadb230122a80b424568566f5ea1b8
khasanjonovich <khasanjonovich@gmail.com>
2022-05-22 15:32:08 +0900

    working with string only

--- Advanced Programming/assignment 3/main.cpp
+++ Advanced Programming/assignment 3/main.cpp
@@ -11,50 +11,24 @@ enum DNA
     G
 };
 
-void get_input(std::string *arr, size_t *len)
-{
-    using namespace std;
-    regex r("[0,1,2,3, A,C,U,G, a,c,u,g]");
-    string str;
-
-    getline(cin, str);
-    str += " ";
-
-    string space_delimiter = " ";
-    size_t pos = 0;
-
-    while ((pos = str.find(space_delimiter)) != string::npos)
-    {
-        if (regex_match(str.substr(0, pos), r))
-        {
-            arr[*len] = str.substr(0, pos);
-            (*len)++;
-        }
-
-        str.erase(0, pos + space_delimiter.length());
-    }
-}
-
 int main()
 {
     using namespace std;
-    string arr1[1000];
-    size_t len1 = 0;
-    string arr2[1000];
-    size_t len2 = 0;
-    get_input(arr1, &len1);
-    get_input(arr2, &len2);
+    string str1;
+    string str2;
+    getline(cin, str1);
+    getline(cin, str2);
+    string bigStr = str1.size() > str2.size() ? str1 : str2;
+    string smallStr = str1.size() < str2.size() ? str1 : str2;
+    size_t bigSize = bigStr.size();
+    size_t smallSize = smallStr.size();
 
-    cout << "1st: ";
-    for (size_t i = 0; i < len1; i++)
-    {
-        cout << arr1[i] << " ";
-    }
+    string whiteSpace(smallSize - 2, ' ');
+    bigStr = whiteSpace + bigStr + whiteSpace;
 
-    cout << "2nd: ";
-    for (size_t i = 0; i < len2; i++)
+    for (size_t i = 0; i < bigSize + smallSize - 1; i += 2)
     {
-        cout << arr2[i] << " ";
+        
     }
 
     return 0;
