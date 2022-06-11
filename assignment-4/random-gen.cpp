8e99e2bcbbc4cb67675f3bfc29f6ba76ac46a792
khasanjonovich <khasanjonovich@gmail.com>
2022-06-12 01:24:26 +0900

    between is ready

--- assignment-4/random-gen.cpp
+++ assignment-4/random-gen.cpp
@@ -1,16 +1,40 @@
 #include "random-gen.h"
+#include <random>
 
 double die::between(double start, double end)
 {
     //* TODO: Die Betwwen
+    //! Generate array of uniformly distributed numbers within a range
+    double *arr = new double(num_sides);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> urd(start, end);
+
+    for (int n = 0; n < num_sides; ++n)
+        arr[n] = urd(gen);
+
+    return arr[roll()];
 }
 
 int biased_die::roll() const
 {
-    size_t count = 0;
     int result = std::rand() % this->num_sides;
     if (result == b)
         return std::rand() % this->num_sides;
     return result;
 };
-double biased_die::between(double start, double end){};
+double biased_die::between(double start, double end)
+{
+
+    //* TODO: Biased Die Betwwen
+    //! Generate array of uniformly distributed numbers within a range
+    double *arr = new double(num_sides);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> urd(start, end);
+
+    for (int n = 0; n < num_sides; ++n)
+        arr[n] = urd(gen);
+
+    return arr[roll()];
+};
