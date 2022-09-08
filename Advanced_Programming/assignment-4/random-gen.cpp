85e2c5e6334282b9c891caa12be1c0313e22808e
khasanjonovich <khasanjonovich@gmail.com>
2022-09-08 12:59:46 +0900

    Reorganize branches

---
+++ Advanced_Programming/assignment-4/random-gen.cpp
@@ -0,0 +1,49 @@
+#include "random-gen.h"
+#include <random>
+
+double die::between(double a, double b)
+{
+    //! Generate array of uniformly distributed numbers within a range
+    double max_v = num_sides;
+    double min_v = 1;
+    double ans = (b - a) * roll() / (max_v - min_v) + a;
+    return ans;
+}
+
+int biased_die::roll() const
+{
+    int result = std::rand() % this->num_sides;
+    if (result == b)
+        return std::rand() % this->num_sides;
+    return result;
+};
+double biased_die::between(double a, double b)
+{
+    //! Generate array of uniformly distributed numbers within a range
+    double max_v = num_sides;
+    double min_v = 1;
+
+    double ans = (b - a) * roll() / (max_v - min_v) + a;
+    return ans;
+};
+
+//
+// template <class T>
+// double predetermined<T>::between(double a, double b)
+// {
+//     //             (b - a)(x - min)
+//     //     f(x) = ------------------ + a = 4*1/5-2
+//     //                  max -min
+//     //
+//     //     max = 10 / min = 5
+//     //     a = -2 / b = 2
+//     //     x = 6
+
+//     double min_v = arr[0];
+//     double max_v = arr[n - 1];
+//     // std::cout << "MAX_V: " << max_v << std::endl;
+
+//     double ans = (b - a) * (arr[step] - min_v) / (max_v - min_v) + a;
+//     step++;
+//     return ans;
+// }
