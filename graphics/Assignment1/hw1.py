c06deeff66b7e961753ccaf1c9c6de3a7d412be9
Azizbek <khasanjonovich@gmail.com>
2022-09-29 06:50:29 +0900

    triangle is ready

--- graphics/Assignment1/hw1.py
+++ graphics/Assignment1/hw1.py
@@ -17,45 +17,8 @@ class Polygon:
 
 # TODO: define your polygons here
 
-VERTEX_SHADER = '''
-#version 330
 
-in vec4 position;
-void main(){
-    gl_Position = position;
-}
-'''
-
-FRAGNMENT_SHADER = '''
-#version 330
-
-void main(){
-    gl_FragColor = vec4(0.2f, 0.6f, 0.8f, 1.0f);
-}
-'''
 shaderProgram = None
-
-
-def initialize():
-    global VERTEX_SHADER
-    global FRAGNMENT_SHADER
-    global shaderProgram
-
-    vertexshader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
-    fragmentshader = shaders.compileShader(
-        FRAGNMENT_SHADER, GL_FRAGMENT_SHADER)
-
-    shaderProgram = shaders.compileProgram(vertexshader, fragmentshader)
-    triangle = [0.0, 0.1, 0.0, -0.1, -0.1, 0.0, 0.1, -0.1, 0.0]
-    triangle = np.array(triangle, dtype=np.float32)
-
-    VBO = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO)
-    glBufferData(GL_ARRAY_BUFFER, triangle.nbytes, triangle, GL_STATIC_DRAW)
-
-    position = glGetAttribLocation(shaderProgram, "position")
-    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, None)
-    glEnableVertexAttribArray(position)
 
 
 class Triangle(Polygon):
@@ -63,7 +26,40 @@ class Triangle(Polygon):
         super().__init__()
 
     def draw(self):
-        pass
+        VERTEX_SHADER = '''
+        #version 330
+
+        in vec4 position;
+        void main(){
+            gl_Position = position;
+        }
+        '''
+
+        FRAGNMENT_SHADER = '''
+        #version 330
+
+        void main(){
+            gl_FragColor = vec4(0.2f, 0.6f, 0.8f, 1.0f);
+        }
+        '''
+        global shaderProgram
+
+        vertexshader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
+        fragmentshader = shaders.compileShader(
+            FRAGNMENT_SHADER, GL_FRAGMENT_SHADER)
+
+        shaderProgram = shaders.compileProgram(vertexshader, fragmentshader)
+        triangle = [0.0, 0.1, 0.0, -0.1, -0.1, 0.0, 0.1, -0.1, 0.0]
+        triangle = np.array(triangle, dtype=np.float32)
+
+        VBO = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, VBO)
+        glBufferData(GL_ARRAY_BUFFER, triangle.nbytes,
+                     triangle, GL_STATIC_DRAW)
+
+        position = glGetAttribLocation(shaderProgram, "position")
+        glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, None)
+        glEnableVertexAttribArray(position)
 
 
 class Rectangle(Polygon):
@@ -92,7 +88,6 @@ class Viewer:
         glMatrixMode(GL_MODELVIEW)
 
         # TODO: visualize your polygons here
-        # * TRIANGLE
         glUseProgram(shaderProgram)
         glDrawArrays(GL_TRIANGLES, 0, 3)
         glUseProgram(0)
@@ -132,7 +127,9 @@ class Viewer:
         glutInitWindowSize(800, 600)
         glutInitWindowPosition(0, 0)
         glutCreateWindow("CS471 Computer Graphics #1")
-        initialize()
+        # * TRIANGLE
+        t = Triangle()
+        t.draw()
         glutDisplayFunc(self.display)
         glutKeyboardFunc(self.keyboard)
         glutSpecialFunc(self.special)
