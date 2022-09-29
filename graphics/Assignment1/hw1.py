fb9f74502268bdb249e4c4a0e8f4be70d08884dc
Azizbek <khasanjonovich@gmail.com>
2022-09-29 20:58:08 +0900

    Rectangle

--- graphics/Assignment1/hw1.py
+++ graphics/Assignment1/hw1.py
@@ -5,6 +5,7 @@ from OpenGL.GL import shaders
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import numpy as np
+from math import cos, sin, pi
 
 
 class Polygon:
@@ -67,7 +68,80 @@ class Rectangle(Polygon):
         super().__init__()
 
     def draw(self):
-        pass
+        # Positions           #Colors
+        rectangle = [-0.1, 0.1, 0,   0.2, 0.6, 0.8,
+                     0.1, 0.1, 0,     0.2, 0.6, 0.8,
+                     0.1, -0.1, 0,      0.2, 0.6, 0.8,
+                     -0.1, -0.1, 0,     0.2, 0.6, 0.8, ]
+        # convert to 32bit float
+
+        rectangle = np.array(rectangle, dtype=np.float32)
+
+        # Creating Indices
+
+        indices = [0, 1, 2,
+                   2, 3, 0]
+
+        indices = np.array(indices, dtype=np.uint32)
+        VERTEX_SHADER = """
+
+            #version 330
+
+            in vec3 position;
+            in vec3 color;
+            out vec3 newColor;
+
+            void main() {
+
+            gl_Position = vec4(position, 1.0);
+            newColor = color;
+
+            }
+        """
+
+        FRAGMENT_SHADER = """
+            #version 330
+
+            in vec3 newColor;
+            out vec4 outColor;
+
+            void main() {
+
+            outColor = vec4(newColor, 1.0f);
+
+            }
+        """
+
+        shader = shaders.compileProgram(shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER),
+                                        shaders.compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER))
+
+        # Create Buffer object in gpu
+        VBO = glGenBuffers(1)
+
+        # Create EBO
+        EBO = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW)
+
+        # Bind the buffer
+        glBindBuffer(GL_ARRAY_BUFFER, VBO)
+        glBufferData(GL_ARRAY_BUFFER, 96, rectangle, GL_STATIC_DRAW)
+
+        # get the position from  shader
+        position = glGetAttribLocation(shader, 'position')
+        glVertexAttribPointer(position, 3, GL_FLOAT,
+                              GL_FALSE, 24, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(position)
+
+        # get the color from  shader
+        color = glGetAttribLocation(shader, 'color')
+        glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE,
+                              24, ctypes.c_void_p(12))
+        glEnableVertexAttribArray(color)
+
+        glUseProgram(shader)
+
+        glClearColor(0.0, 0.0, 0.0, 1.0)
 
 
 class Ellipse(Polygon):
@@ -75,10 +149,29 @@ class Ellipse(Polygon):
         super().__init__()
 
     def draw(self):
-        pass
+        i = None
+        triangleAmount = 20
+        glColor3f(0.2, 0.6, 0.8)
+        x1 = 320
+        y1 = 240
+        r1 = 75
+        x = 100
+        y = 100
+        glBegin(GL_TRIANGLE_FAN)
+        glVertex2f(100, 100)  # center of circle
+        for i in range(0, triangleAmount+1):
+            glVertex2f(
+                1.4 * (x + (100 * cos(i * 2*pi / triangleAmount))),
+                1.111 * (y + (100 * sin(i * 2*pi / triangleAmount)))
+            )
+        glutSwapBuffers()
 
 
 class Viewer:
+    key = None
+    x = None
+    y = None
+
     def __init__(self):
         pass
 
@@ -88,13 +181,33 @@ class Viewer:
         glMatrixMode(GL_MODELVIEW)
 
         # TODO: visualize your polygons here
-        glUseProgram(shaderProgram)
-        glDrawArrays(GL_TRIANGLES, 0, 3)
-        glUseProgram(0)
+
+        if self.key == b"1":
+            # * TRIANGLE
+            t = Triangle()
+            t.draw()
+            glUseProgram(shaderProgram)
+            glDrawArrays(GL_TRIANGLES, 0, 3)
+            glUseProgram(0)
+
+        elif self.key == b"2":
+            r = Rectangle()
+            r.draw()
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,  None)
+
+        elif self.key == b"3":
+            e = Ellipse()
+            e.draw()
+            # glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,  None)
+
         glutSwapBuffers()
 
     def keyboard(self, key, x, y):
         print(f"keyboard event: key={key}, x={x}, y={y}")
+        self.x = x
+        self.y = y
+        self.key = key
+
         if glutGetModifiers() & GLUT_ACTIVE_SHIFT:
             print("shift pressed")
         if glutGetModifiers() & GLUT_ACTIVE_ALT:
@@ -127,10 +240,16 @@ class Viewer:
         glutInitWindowSize(800, 600)
         glutInitWindowPosition(0, 0)
         glutCreateWindow("CS471 Computer Graphics #1")
-        # * TRIANGLE
-        t = Triangle()
-        t.draw()
+
+        # r = Rectangle()
+        # r.draw()
+        # glClear(GL_COLOR_BUFFER_BIT)
+
+        # Draw Triangle
+        # glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,  None)
+
         glutDisplayFunc(self.display)
+
         glutKeyboardFunc(self.keyboard)
         glutSpecialFunc(self.special)
         glutMouseFunc(self.mouse)
