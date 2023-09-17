22f7b1865de543b6193b52d948023489023c0a62
typosbro <typosbro@proton.me>
2023-09-17 18:47:33 +0900

    encryption is done

--- security/hw1/rsa_program.py
+++ security/hw1/rsa_program.py
@@ -1,10 +1,9 @@
 import argparse
 import math
 
-# * RSA Key Generation
+# *1. RSA Key Generation
 
 
-# Euclid's algorithm for determining the greatest common divisor
 def gcd(a, b):
     if a == 0:
         return b
@@ -29,49 +28,142 @@ def generate(p, q):
         # print(f'i: {i}')
         if (math.fmod((e*i), phi) == 1):
             d = i
-
     return (e, d, mod, phi)
 
 
-# Driver code
-parser = argparse.ArgumentParser(description='RSA Key Generator')
-parser.add_argument('--generate-key', action='store_true',
-                    help='Generate RSA key')
-parser.add_argument('--p', type=int, help='Prime number p')
-parser.add_argument('--q', type=int, help='Prime number q')
-
-args = parser.parse_args()
-
-if args.generate_key:
-    p = args.p
-    q = args.q
-    # generate keys using p and q
-    (encrypt, decrypt, mod, phi) = generate(p, q)
+def print_generated_keys(e, d, mod, phi):
+    print(f'RSA key pair generated:')
+    print(f'n={mod}')
+    print(f'e={e}')
+    print(f'd={d}')
+    print(f'phi={phi}')
 
 
-print(f'RSA key pair generated:')
-print(f'n={mod}')
-print(f'e={encrypt}')
-print(f'd={decrypt}')
-print(f'phi={phi}')
+def print_generated_keys_to_file(e, d, mod):
+    # Write the public key to a file
+    with open('public_key.txt', 'w') as f:
+        f.write(f'n={mod}\n')
+        f.write(f'e={e}')
+        f.close()
+    # Write the private key to a file
+    with open('private_key.txt', 'w') as f:
+        f.write(f'n={mod}\n')
+        f.write(f'd={d}')
+        f.close()
 
 
-# Write the public key to a file
-with open('public_key.txt', 'w') as f:
-    f.write(f'n={mod}\n')
-    f.write(f'e={encrypt}')
-    f.close()
-# Write the private key to a file
-with open('private_key.txt', 'w') as f:
-    f.write(f'n={mod}\n')
-    f.write(f'd={decrypt}')
-    f.close()
+# *2. RSA Encryption & Descryption
 
-
-# * RSA Encryption & Descryption
 
 def char_to_hex(char):
     return hex(ord(char))
 
 
-# print(char_to_hex('a'))  # Output: 0x61
+def read_plaintext(plaintext):
+    with open(plaintext, 'r') as file:
+        data = file.read()
+    return data
+
+
+def read_and_parse_public_key(public_key):
+    obj = {}
+    with open(public_key, 'r') as file:
+        for line in file:
+            # Remove any newline characters
+            line = line.strip()
+            # Split the line on the equals sign
+            key, value = line.split('=')
+            obj[key] = value
+    return obj
+
+
+def print_ciphertext(ciphertext):
+    print(f'Ciphertext: {ciphertext}')
+
+# print hex ciphertext list to terminal
+
+
+def print_ciphertext(ciphertext):
+    txt = ""
+    for i in ciphertext:
+        if i == ciphertext[-1]:
+            txt += f'{i}'
+            break
+        txt += f'{i} '
+    print(txt)
+
+
+def write_ciphertext_to_file(ciphertext, output):
+    with open(output, 'w') as f:
+        for i in ciphertext:
+            if i == ciphertext[-1]:
+                f.write(f'{i}')
+                break
+            f.write(f'{i} ')
+        f.close()
+
+
+# Driver code
+parser = argparse.ArgumentParser(description='RSA Key Generator')
+# Add arguments
+# Args for 1st part of the assignment
+parser.add_argument('--generate-key', action='store_true',
+                    help='Generate RSA key')
+parser.add_argument('--p', type=int, help='Prime number p')
+parser.add_argument('--q', type=int, help='Prime number q')
+
+# Args for 2nd part of the assignment
+parser.add_argument("--encrypt", type=str, help="Path to the plaintext file.")
+parser.add_argument("--public-key", type=str,
+                    help="Path to the public key file.")
+parser.add_argument("--output", type=str,
+                    help="Path to the output ciphertext file.")
+
+# Args for 3rd part of the assignment
+
+# Parse the arguments
+args = parser.parse_args()
+
+if args.generate_key:
+    # Args for 1st part of the assignment
+    p = args.p
+    q = args.q
+    # generate keys using p and q
+    (e, d, mod, phi) = generate(p, q)
+    # print keys
+    print_generated_keys(e, d, mod, phi)
+    # print keys to file
+    print_generated_keys_to_file(e, d, mod)
+
+if args.encrypt:
+    # Args for 2nd part of the assignment
+    encrypt = args.encrypt
+    public_key = args.public_key
+    output = args.output
+
+    # read plaintext
+    plaintext = read_plaintext(encrypt)
+    # read public key
+    public_key_obj = read_and_parse_public_key(public_key)
+    plaintext_ascii = [ord(c) for c in plaintext]
+    # encrypt plaintext
+    ciphertext = []
+    for i in plaintext_ascii:
+        ciphertext.append(
+            pow(i, int(public_key_obj['e']), int(public_key_obj['n'])))
+    hex_ciphertext = [hex(c) for c in ciphertext]
+    print_ciphertext(hex_ciphertext)
+    # write ciphertext to file
+    write_ciphertext_to_file(hex_ciphertext, output)
+
+
+# if args.decrypt:
+#     # Args for 3rd part of the assignment
+#     decrypt = args.decrypt
+#     private_key = args.private_key
+#     output = args.output
+
+    # read ciphertext
+    # ciphertext = read_ciphertext(decrypt)
+    # read private key
+    # private_key_obj = read_and_parse_private_key(private_key)
